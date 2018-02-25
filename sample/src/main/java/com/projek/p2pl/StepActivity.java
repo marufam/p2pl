f1d1a0b3a4a289367a8d9bc13645016efbf9c9b8
artdeffend <artdeffend@gmail.com>
2018-02-25 20:27:37 +0700

    p2pl projek

---
+++ sample/src/main/java/com/projek/p2pl/StepActivity.java
@@ -0,0 +1,35 @@
+package com.projek.p2pl;
+
+import android.os.Bundle;
+
+import com.github.fcannizzaro.materialstepper.AbstractStep;
+import com.github.fcannizzaro.materialstepper.style.TabStepper;
+
+public class StepActivity extends TabStepper {
+
+    private int i = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        boolean linear = getIntent().getBooleanExtra("linear", false);
+
+        setErrorTimeout(1500);
+        setLinear(linear);
+        setTitle("Isi Form");
+        setAlternativeTab(true);
+
+        addStep(createFragment(new Page1()));
+        addStep(createFragment(new Page1()));
+
+        super.onCreate(savedInstanceState);
+    }
+
+    private AbstractStep createFragment(AbstractStep fragment) {
+        Bundle b = new Bundle();
+        b.putInt("position", i++);
+        fragment.setArguments(b);
+        return fragment;
+    }
+
+}
