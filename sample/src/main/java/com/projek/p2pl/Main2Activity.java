4c0b7b3766799b6654f2d4346b64311bc8e65a61
artdeffend <artdeffend@gmail.com>
2018-02-27 20:05:45 +0700

    map

--- sample/src/main/java/com/projek/p2pl/Main2Activity.java
+++ sample/src/main/java/com/projek/p2pl/Main2Activity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -13,19 +15,26 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 import com.fcannizzaro.p2pl.R;
 
 public class Main2Activity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
-
+    NavigationView mNavigationView;
+    private View navHeader;
+//    TextView email,username;
+    FragmentManager mFragmentManager;
+    FragmentTransaction mFragmentTransaction;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        mFragmentManager = getSupportFragmentManager();
 
+        mNavigationView = (NavigationView) findViewById(R.id.nav_view);
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -35,6 +44,9 @@ public class Main2Activity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+        mFragmentTransaction = mFragmentManager.beginTransaction(); ///fragment transaction ini digunakan jika ingin mengganti fragment
+        mFragmentTransaction.replace(R.id.containerView, new MapFragment()).commit();
+
     }
 
     @Override
--- sample/src/main/java/com/projek/p2pl/Page1.java
+++ sample/src/main/java/com/projek/p2pl/Page1.java
@@ -6,7 +6,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
-import com.fcannizzaro.p2pl.R;
+import com.projek.p2pl.R;
 import com.github.fcannizzaro.materialstepper.AbstractStep;
 
 /**
