fab61202f81e111b333c4db7be585999f5d93e6f
artdeffend <artdeffend@gmail.com>
2018-03-06 22:16:15 +0700

    Check INternet Connection

--- sample/src/main/java/com/projek/p2pl/SplashScreen.java
+++ sample/src/main/java/com/projek/p2pl/SplashScreen.java
@@ -1,10 +1,14 @@
 package com.projek.p2pl;
 
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
 import java.io.File;
 
@@ -20,13 +24,26 @@ public class SplashScreen extends AppCompatActivity {
                     @Override
                     public void run() {
                 /* Create an Intent that will start the Menu-Activity. */
-                        Intent mainIntent = new Intent(SplashScreen.this, Main2Activity.class);
+                if(isNetworkAvailable()){
+                    Toast.makeText(SplashScreen.this, "Internet Availabel", Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(SplashScreen.this, "Internet Not Available", Toast.LENGTH_SHORT).show();
+                }
+                Intent mainIntent = new Intent(SplashScreen.this, Main2Activity.class);
                         SplashScreen.this.startActivity(mainIntent);
                         SplashScreen.this.finish();
+
                     }
                 }, SPLASH_DISPLAY_LENGTH);
 
 
 
+    }
+
+    private boolean isNetworkAvailable() {
+        ConnectivityManager connectivityManager
+                = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+        return activeNetworkInfo != null && activeNetworkInfo.isConnected();
     }
 }
